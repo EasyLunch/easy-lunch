e7067ff5b428d0681fd0f9f1a8f59c1ea62de464
Pablo <bronstein300@gmail.com>
2026-06-03 18:13:47 -0300

    feat: insumos reales con merma y coccion

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Resetea los insumos si la versión de datos cambió
+const DATA_VERSION = '2026-06-03-v1'
+if (localStorage.getItem('el_data_version') !== DATA_VERSION) {
+  localStorage.removeItem('el_insumos')
+  localStorage.setItem('el_data_version', DATA_VERSION)
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
