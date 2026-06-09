470b994a498aca1c869104b60ee5ff9fececc964
Pablo <bronstein300@gmail.com>
2026-06-08 22:37:25 -0300

    fix: corregir clave anon de Supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://xlwcozznliafhouhqjzl.supabase.co',
-  'sb_publishable_kyFxSpbS4PZiZIlME5kOPQ_G7W2DopK'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhsd2NvenpubGlhZmhvdWhxanpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5NTkwMzAsImV4cCI6MjA5NjUzNTAzMH0.i-GTNnGK_5GMUum_tmUKIiX4NUkmEiJovK_M7BwGFfg'
 )
